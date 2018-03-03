2de8f6015b123ef08313b1a9980f8d3723aa498b
Teascade <teascade@gmail.com>
2018-03-03 19:01:23 +0200

    Add Display and a white screen

---
+++ src/display.rs
@@ -0,0 +1,59 @@
+
+use glutin::{ContextBuilder, DeviceId, ElementState, Event, EventsLoop, GlContext, GlWindow,
+             KeyboardInput, VirtualKeyCode, WindowBuilder, WindowEvent};
+use gl;
+
+pub struct Display {
+    window: GlWindow,
+    events_loop: EventsLoop,
+}
+
+impl Display {
+    pub fn new() -> Display {
+        let events_loop = EventsLoop::new();
+        let window = WindowBuilder::new()
+            .with_title("title")
+            .with_dimensions(1280, 720);
+        let context = ContextBuilder::new()
+            .with_vsync(true);
+        let gl_window;
+        match GlWindow::new(window, context, &events_loop) {
+            Ok(window) => gl_window = window,
+            Err(err) => panic!(err)
+        }
+
+        unsafe {
+            if let Err(err) = gl_window.make_current() {
+                panic!(err);
+            }
+            gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+            gl::ClearColor(1.0, 1.0, 1.0, 1.0);
+        }
+
+        Display {
+            window: gl_window,
+            events_loop: events_loop,
+        }
+    }
+
+    pub fn refresh(&mut self) -> bool {
+        let mut running = true;
+
+        self.window.swap_buffers().unwrap();
+
+        self.events_loop.poll_events(|event|
+            match event {
+                Event::WindowEvent {event, ..} => {
+                    match event {
+                        WindowEvent::Closed  =>  {
+                            running = false;
+                        }
+                        _ => ()
+                    }
+                },
+                _ => ()
+            }
+        );
+        running
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 
 extern crate glutin;
+extern crate gl;
+extern crate libc;
+
+mod display;
+
+use display::Display;
 
 fn main() {
     println!("Hello, world!");
 
+    let mut display: Display = Display::new();
+
+    while display.refresh() {
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+    }
 }
